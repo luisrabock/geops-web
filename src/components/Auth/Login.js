080d529dd6bf835b2a7483ec84afe7417fe13fbd
Luis <luis@rocketlab.com.br>
2020-06-14 16:58:58 -0300

    feat: setup receive current user from the api
    setup receive current user from the api and send value to context

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,19 +1,49 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useContext } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { GraphQLClient, ClientError } from 'graphql-request';
+import { GoogleLogin } from 'react-google-login';
+
+import appContext from '../../contexts/context';
 // import Typography from "@material-ui/core/Typography";
 
+const ME_QUERY = `
+{
+  me{
+      _id
+      name
+      email
+      picture
+  }
+}`;
+
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+    const { dispatch } = useContext(appContext);
+    const onSuccess = async googleUser => {
+        const idToken = googleUser.getAuthResponse().id_token;
+        const client = new GraphQLClient('http://localhost:4000/graphql', {
+            headers: { authorization: idToken },
+        });
+        const data = await client.request(ME_QUERY);
+
+        dispatch({ type: 'LOGIN_USER', payload: data.me });
+    };
+    return (
+        <GoogleLogin
+            clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
+            onSuccess={onSuccess}
+            isSignedIn={true}
+        />
+    );
 };
 
 const styles = {
-  root: {
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center"
-  }
+    root: {
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
 };
 
 export default withStyles(styles)(Login);
